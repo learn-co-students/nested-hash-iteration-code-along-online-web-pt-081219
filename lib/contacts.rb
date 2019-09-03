3f89abf68641c656cd484eb5c5777a48c4903ef0
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-09-03 19:54:52 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,17 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+contacts.each do |person, contact_details_hash|
+  # binding.pry
+end
+end
+# pry out 
 
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
 end
